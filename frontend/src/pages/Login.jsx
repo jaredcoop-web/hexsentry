c29332b86ab5e205e7f893494c00019ab5cd1b49
Jared <jareddcoop8@gmail.com>
2026-09-25 23:50:14 -0500

    Resize login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -47,7 +47,7 @@ export default function Login({ onLogin, onBack, onForgotPassword }) {
     <div style={{
       flex: 1,
       backgroundImage: position === 'left' ? 'url(/molecule.png)' : 'url(/logo.png)',
-      backgroundSize: '75%',
+      backgroundSize: '85%',
       backgroundPosition: 'center center',
       backgroundRepeat: 'no-repeat',
       minHeight: '100vh',
