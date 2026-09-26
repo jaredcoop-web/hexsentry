fc932ab8e2659aeb7945c18f59496e08f669a685
Jared <jareddcoop8@gmail.com>
2026-09-25 23:54:19 -0500

    Make the login images a little bigger

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -47,7 +47,7 @@ export default function Login({ onLogin, onBack, onForgotPassword }) {
     <div style={{
       flex: 1,
       backgroundImage: position === 'left' ? 'url(/molecule.png)' : 'url(/logo.png)',
-      backgroundSize: '85%',
+      backgroundSize: '93%',
       backgroundPosition: 'center center',
       backgroundRepeat: 'no-repeat',
       minHeight: '100vh',
